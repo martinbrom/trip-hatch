6983fe7275b6063698b5ab8e95443b06951bf0b4
Martin Brom <martinbrom97@gmail.com>
2017-10-21 16:40:05 +0200

    gulpfile watch fix & error handling, simple layout + scss

--- gulpfile.js
+++ gulpfile.js
@@ -12,8 +12,13 @@ var scripts = [
 
 var styles = [
     'bower_components/bootstrap/dist/css/bootstrap.css',
-    'resources/assets/sass/*.scss'
+    'resources/assets/scss/*.scss'
 ];
+
+function handleError(error) {
+    console.log(error.toString());
+    this.emit('end');
+}
 
 gulp.task('js', function(){
     gulp.src(scripts)
@@ -25,13 +30,14 @@ gulp.task('scss', function(){
     gulp.src(styles)
         .pipe(concat('app.scss'))
         .pipe(sass())
+        .on('error', handleError)
         .pipe(cleanCSS())
         .pipe(gulp.dest('public/css/'));
 });
 
 gulp.task('watch', ['js', 'scss'], function() {
-    gulp.watch(['application/assets/js/*.js'], ['js']);
-    gulp.watch(['application/assets/sass/*.scss'], ['css']);
+    gulp.watch(['resources/assets/js/*.js'], ['js']);
+    gulp.watch(['resources/assets/scss/*.scss'], ['scss']);
 });
 
 gulp.task('bundle', ['js','scss'], function() {
