48bb52a2d6a94d1b36f177f6a1c9652e9a5ed7aa
Martin Brom <martinbrom97@gmail.com>
2017-10-25 15:43:11 +0200

    trip overview

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,11 +1,30 @@
 $(document).ready(function () {
 
+    // *************
+    // * VARIABLES *
+    // *************
+
     // time of animations in milliseconds
     var animation_time_very_fast = 200;
     var animation_time_fast      = 350;
     var animation_time_normal    = 500;
     var animation_time_slow      = 750;
     var animation_time_very_slow = 1000;
+
+
+    // *************
+    // * FUNCTIONS *
+    // *************
+
+    // scrolls down to the top of given page at given animation speed
+    function scrollToPageID(page_id, scroll_speed) {
+        $('body,html').animate({ scrollTop: window.innerHeight * page_id}, scroll_speed);
+    }
+
+
+    // ******************
+    // * EVENT HANDLING *
+    // ******************
 
     // when the window is scrolled for more than 30px
     // display "back-to-top" button
@@ -17,11 +36,6 @@ $(document).ready(function () {
         }
     });
 
-    // scrolls down to the top of given page at given animation speed
-    function scrollToPageID(page_id, scroll_speed) {
-        $('body,html').animate({ scrollTop: window.innerHeight * page_id}, scroll_speed);
-    }
-
     $("button.back-to-top").click(function () {
         scrollToPageID($(this).attr("data-scroll-target"), animation_time_fast);
     });
@@ -29,4 +43,9 @@ $(document).ready(function () {
     $("a.scroll-down").click(function () {
         scrollToPageID($(this).attr("data-scroll-target"), animation_time_slow);
     });
+
+    $("a.day-collapse-btn").click(function () {
+        $(this).find(">:first-child").toggleClass("fa-plus");
+        $(this).find(">:first-child").toggleClass("fa-minus");
+    })
 });
