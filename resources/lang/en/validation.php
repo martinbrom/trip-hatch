032c31170a5f3be8b95c1fd64966d9f5057a1308
Martin Brom <martinbrom97@gmail.com>
2017-11-15 22:15:00 +0100

    fix unchanged validation message parameter name

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -3,7 +3,7 @@
 return [
     'login_email' => [
         'email' => 'Login email must be a valid email address',
-        'maxLen' => 'Login email must be at most :param characters long'
+        'maxLen' => 'Login email must be at most :p1 characters long'
     ],
     'a' => [
         'between' => 'Parameter A must be between :p1 and :p2',
